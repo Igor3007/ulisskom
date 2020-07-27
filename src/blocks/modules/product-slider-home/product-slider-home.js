df8f7994e2fe3d2f17fb4b20bdaf9e7cd376e0ac
igor3007 <dok307@gmail.com>
2020-07-27 23:14:04 +0300

    миникарточка товара

--- src/blocks/modules/product-slider-home/product-slider-home.js
+++ src/blocks/modules/product-slider-home/product-slider-home.js
@@ -7,10 +7,29 @@ $(document).ready(function () {
     const productSlider = $('.product-slider__tabscontainer.owl-carousel');
     const productSliderNext = $('.product-slider__nav-next');
     const productSliderPrev = $('.product-slider__nav-prev');
+    const productSliderTabs = $('.product-slider__list');
+    const productSliderSlide = $('.product-slider__slide');
 
     productSlider.owlCarousel({
         items: 5,
-        loop: true
+        loop: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            580: {
+                items: 2
+            },
+            760: {
+                items: 3
+            },
+            1200: {
+                items: 4
+            },
+            1440: {
+                items: 5
+            }
+        }
     })
 
     productSliderNext.on('click', function () {
@@ -20,6 +39,38 @@ $(document).ready(function () {
         productSlider.trigger('prev.owl.carousel')
     })
 
+    /* filter slider-2 */
+    productSliderTabs.on('click', 'li', function (e) {
+
+        e.preventDefault();
+
+        productSliderTabs.find('li').removeClass('active')
+        $(this).addClass('active')
+
+        /*получаем id текущей группы*/
+        var selectGroup = $(this).data('filter-id');
+
+        /* находим первый элем в слайдере с таким id и получаем index */
+        var indexElem = $('.product-slider__slide[data-filter=' + selectGroup + ']').parent().index()
+
+        /*переходим к группе*/
+        productSlider.trigger('to.owl.carousel', [indexElem, 200, true]);
+
+    })
+
+    productSlider.on('changed.owl.carousel', function (event) {
+
+        var indexElemSlider = event.item.index;
+
+        var ElemSlider = $('.product-slider__tabscontainer .owl-stage > div')
+            .eq(indexElemSlider)
+            .children()
+            .data('filter');
+
+        productSliderTabs.find('li').removeClass('active');
+        $('[data-filter-id=' + ElemSlider + ']').addClass('active');
+
+    })
 
 
 })
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,5 +1,7 @@
 import "%modules%/header/header";
 import "%modules%/footer/footer";
+/* home */
 import "%modules%/banner-home/banner-home";
 import "%modules%/slider-home/slider-home";
 import "%modules%/product-slider-home/product-slider-home"; 
+import "%modules%/minicard/minicard"; 
