5955241b52041fbc9171df9f8827045dda4ce619
Igor3007 <50610898+Igor3007@users.noreply.github.com>
2020-08-04 14:18:13 +0300

    Страница о компании, партнеры

---
+++ src/blocks/modules/menu-info/menu-info.js
@@ -0,0 +1,13 @@
+modules.define('menu-info', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
